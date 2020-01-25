2bb8faac6cdf8021c6de40d72348b83145544cd4
jackmoody11 <jackmoody@unc.edu>
2020-01-25 17:40:04 -0500

    Update config

--- config.py
+++ config.py
@@ -3,4 +3,9 @@ import os
 
 class Config:
     TEMPLATES_AUTO_RELOAD = True
-    FREEZER_DESTINATION = os.path.dirname(os.path.abspath(__file__))
+    # Assumes this file is in project's main directory
+    PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
+    # Build static files here
+    FREEZER_DESTINATION = os.path.join(PROJECT_ROOT, 'build')
+    BASE_URL = "https://jackmoody11.github.io"
+    GITHUB_REPO = "https://github.com/jackmoody11/jackmoody11.github.io"
