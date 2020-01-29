01b95a8b2b0465d164ed42b9f6976d48486b3d5a
jackmoody11 <jackmoody@unc.edu>
2020-01-29 14:19:05 -0500

    Change project root and build path variables

--- jackmoody11/config.py
+++ jackmoody11/config.py
@@ -4,8 +4,8 @@ import os
 class Config:
     TEMPLATES_AUTO_RELOAD = True
     # Assumes this file is in project's main directory
-    PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
+    PROJECT_ROOT = os.path.dirname(os.path.join(os.path.abspath(__file__), '..'))
     # Build static files here
-    FREEZER_DESTINATION = os.path.join(PROJECT_ROOT, '../build')
+    FREEZER_DESTINATION = os.path.join(PROJECT_ROOT, 'build')
     BASE_URL = "https://jackmoody11.github.io"
     GITHUB_REPO = "https://github.com/jackmoody11/jackmoody11.github.io"
