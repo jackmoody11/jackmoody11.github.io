096110b2e3453f1dc53af0170e5a1487fd93b873
jackmoody11 <jackmoody@unc.edu>
2020-03-23 21:50:22 -0400

    BUG: Add CNAME to properly deploy

--- jackmoody11/cli.py
+++ jackmoody11/cli.py
@@ -1,31 +1,30 @@
 from flask import current_app
 from flask_frozen import Freezer
 import os
+import shutil
 
 from flask.cli import AppGroup
 
 build_cli = AppGroup('build')
 
 
-@build_cli.command('build')
 def build():
     """Builds the static files."""
     print("Freezing it up! Brr...")
     freezer = Freezer(current_app)
     freezer.freeze()  # Freezes the project to build/
-    # print('Copying CNAME...')
-    # cname = os.path.join(HERE, 'CNAME')
-    # shutil.copyfile(cname, os.path.join(build_dir, 'CNAME'))
+    print('Copying CNAME...')
+    top_level_dir = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+    cname = os.path.join(top_level_dir, 'CNAME')
+    shutil.copyfile(cname, os.path.join(
+        current_app.config['FREEZER_DESTINATION'], 'CNAME'))
     print('...done')
 
 
 @build_cli.command('deploy')
 def deploy():
     """Deploys the site to GitHub Pages."""
-    print('Freezing it up! Brr...')
-    freezer = Freezer(current_app)
-    freezer.freeze()
-    print('...done')
+    build()
     print('Deploying to GitHub pages...')
     build_dir = current_app.config['FREEZER_DESTINATION']
     command = 'ghp-import -b master -m "[deploy] Build" '
