29d351806bf46ac4c594f09c6aaac1089de4e2b5
jackmoody11 <jackmoody@unc.edu>
2020-04-03 22:41:35 -0400

    ENH: Add 404 page

---
+++ src/pages/404.js
@@ -0,0 +1,14 @@
+import React from "react";
+import Layout from "../components/layout";
+
+const Page404 = () => (
+  <Layout>
+    <div className="row">
+      <div className="mx-auto">
+        <h1>The page you're looking for can't be found.</h1>
+      </div>
+    </div>
+  </Layout>
+);
+
+export default Page404;
