2d486f93e58f80facbe3cad7cecf3dc7d6173b66
jackmoody11 <jackmoody@unc.edu>
2020-04-05 18:02:49 -0400

    WIP: Dynamically create blog posts

--- src/templates/blog.js
+++ src/templates/blog.js
@@ -1,8 +1,35 @@
 import React from "react";
+import { graphql } from "gatsby";
+import moment from "moment";
+
 import Layout from "../components/layout";
 
-const Blog = () => {
-  return <Layout>This is the blog template</Layout>;
+export const query = graphql`
+  query($slug: String!) {
+    markdownRemark(fields: { slug: { eq: $slug } }) {
+      frontmatter {
+        title
+        date
+      }
+      html
+    }
+  }
+`;
+
+const Blog = props => {
+  return (
+    <Layout>
+      <h1>{props.data.markdownRemark.frontmatter.title}</h1>
+      <p>
+        {moment(props.data.markdownRemark.frontmatter.date).format(
+          "MMM D, YYYY"
+        )}
+      </p>
+      <div
+        dangerouslySetInnerHTML={{ __html: props.data.markdownRemark.html }}
+      ></div>
+    </Layout>
+  );
 };
 
 export default Blog;
