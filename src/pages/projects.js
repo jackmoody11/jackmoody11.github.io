6e0547a671c9ed9c1fd13519c97e94390432645e
jackmoody11 <jackmoody@unc.edu>
2020-04-07 11:45:45 -0400

    CLN: Get rid of unused import

--- src/pages/projects.js
+++ src/pages/projects.js
@@ -2,7 +2,6 @@ import React from "react";
 import projects from "../content/projects.json";
 import ProjectCard from "../components/project_card";
 import Layout from "../components/layout";
-import { withPrefix } from "gatsby";
 
 const ProjectPage = () => (
   <Layout title="Projects">
@@ -10,7 +9,7 @@ const ProjectPage = () => (
     <hr />
     <div className="mb-3" id="tag-filters"></div>
     <div className="card-deck card-columns mb-3">
-      {projects.projects.map(project => (
+      {projects.projects.map((project) => (
         <ProjectCard project={project} />
       ))}
     </div>
