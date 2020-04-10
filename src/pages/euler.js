030c3bd387f71a2de4cb377fcccbda6eab50ca9e
jackmoody11 <jackmoody@unc.edu>
2020-04-10 17:40:55 -0400

    FIX: Import graphql

--- src/pages/euler.js
+++ src/pages/euler.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { useStaticQuery, Link } from "gatsby";
+import { useStaticQuery, Link, graphql } from "gatsby";
 import Layout from "../components/layout";
 
 const EulerMainPage = () => (
