343b48c698da393f01b41cbd0910f0331362d2f1
jackmoody11 <jackmoody@unc.edu>
2020-04-13 17:31:24 -0400

    FIX: Fix footer on mobile

--- src/components/footer.js
+++ src/components/footer.js
@@ -8,7 +8,7 @@ export default () => {
         <div className="col-6 offset-3">
           <span className="text-muted">&copy; Jack Moody 2020</span>
         </div>
-        <div className={"col-3 " + footerStyles.socialIcons}>
+        <div className={"col-3 p-0 " + footerStyles.socialIcons}>
           <a
             href="https://github.com/jackmoody11"
             target="_blank"
