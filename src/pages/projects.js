d154371dc627bdd4ed1860a5200afcd3366be05c
jackmoody11 <jackmoody@unc.edu>
2020-04-16 17:21:32 -0400

    WIP: Start work on project filters

--- src/pages/projects.js
+++ src/pages/projects.js
@@ -3,17 +3,60 @@ import projects from "../content/projects.json";
 import ProjectCard from "../components/projects/project_card";
 import Layout from "../components/layout";
 
-const ProjectPage = () => (
-  <Layout title="Projects">
-    <h1>Projects</h1>
-    <hr />
-    <div className="mb-3" id="tag-filters"></div>
-    <div className="card-deck card-columns mb-3">
-      {projects.projects.map((project) => (
-        <ProjectCard project={project} />
-      ))}
-    </div>
-  </Layout>
-);
+export default class ProjectPage extends React.Component {
+  constructor(props) {
+    super(props);
+    this.allProjects = projects.projects;
+    this.state = {
+      filters: [],
+      filteredProjects: this.allProjects,
+    };
+    this.removeFilter = this.removeFilter.bind(this);
+  }
 
-export default ProjectPage;
+  removeFilter(filter) {
+    this.setState({
+      filters: this.state.filters.filter((f) => f !== filter),
+    });
+    this.updateFilteredProjects();
+  }
+
+  addFilter(filter) {
+    this.setState({
+      filters: this.state.filters.push(filter),
+    });
+    this.updateFilteredProjects();
+  }
+
+  updateFilteredProjects() {
+    this.setState({
+      filteredProjects: this.allProjects.filter(
+        (project) =>
+          project.tags.filter((tag) => this.state.filters.includes(tag))
+            .length > 0
+      ),
+    });
+  }
+
+  render() {
+    return (
+      <Layout title="Projects">
+        <h1>Projects</h1>
+        <hr />
+        <div className="mb-3" id="tag-filters">
+          {this.state.filters.map((filter) => (
+            <span
+              className="badge badge-light p-1"
+              onClick={this.removeFilter}
+            ></span>
+          ))}
+        </div>
+        <div className="card-deck card-columns mb-3">
+          {this.state.filteredProjects.map((project) => (
+            <ProjectCard project={project} onClick={this.addFilter} />
+          ))}
+        </div>
+      </Layout>
+    );
+  }
+}
