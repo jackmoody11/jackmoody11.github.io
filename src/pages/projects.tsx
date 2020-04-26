440e28c5d70f782b4b1bc3f6e63af4bb72f129a2
jackmoody11 <jackmoody@unc.edu>
2020-04-25 23:48:35 -0400

    WIP: Add filtering when clicking on project tag

--- src/pages/projects.tsx
+++ src/pages/projects.tsx
@@ -15,26 +15,34 @@ export default class ProjectPage extends React.Component {
       filteredProjects: this.allProjects,
     };
     this.removeFilter = this.removeFilter.bind(this);
+    this.addFilter = this.addFilter.bind(this);
+    this.updateFilteredProjects = this.updateFilteredProjects.bind(this);
   }
 
-  removeFilter(filter: any) {
+  removeFilter(e: any) {
+    const filter = e.currentTarget.textContent;
     this.setState({
       filters: this.state.filters.filter((f: any) => f !== filter),
     });
     this.updateFilteredProjects();
   }
 
-  addFilter(filter: any) {
-    this.setState({
-      filters: this.state.filters.push(filter),
-    });
+  addFilter(e: any) {
+    const filter = e.currentTarget.textContent;
+    this.state.filters.push(filter);
     this.updateFilteredProjects();
   }
 
   updateFilteredProjects() {
     this.setState({
       filteredProjects: this.allProjects.filter(
-        (project: { tags: { filter: (arg0: (tag: any) => any) => { (): any; new(): any; length: number; }; }; }) =>
+        (project: {
+          tags: {
+            filter: (
+              arg0: (tag: any) => any
+            ) => { (): any; new (): any; length: number };
+          };
+        }) =>
           project.tags.filter((tag: any) => this.state.filters.includes(tag))
             .length > 0
       ),
@@ -47,16 +55,21 @@ export default class ProjectPage extends React.Component {
         <h1>Projects</h1>
         <hr />
         <div className="mb-3" id="tag-filters">
-          {this.state.filters.map((filter: any) => (
+          {this.state.filters.map((filter: any, index: number) => (
             <span
               className="badge badge-light p-1"
               onClick={this.removeFilter}
+              key={`filter-${index}`}
             ></span>
           ))}
         </div>
         <div className="card-deck card-columns mb-3">
-          {this.state.filteredProjects.map((project: any) => (
-            <ProjectCard project={project} onClick={this.addFilter} />
+          {this.state.filteredProjects.map((project: any, index: number) => (
+            <ProjectCard
+              project={project}
+              onFilterClick={this.addFilter}
+              key={`project-${index}`}
+            />
           ))}
         </div>
       </Layout>
